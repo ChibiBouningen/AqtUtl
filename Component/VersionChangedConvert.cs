97c9d90dbd9cbb12c8ac4af5eb196f835ed9e5b0
ChibiBouningen <84241001+ChibiBouningen@users.noreply.github.com>
2023-08-08 17:51:20 +0900

    バージョン変更時に以前のバージョンを表示するように変更

--- Component/VersionChangedConvert.cs
+++ Component/VersionChangedConvert.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,8 +12,18 @@ namespace AqT_Utl.Component
     {
         public VersionChangedConvert(Version last_version)
         {
+            Version now = Assembly.GetExecutingAssembly().GetName().Version;
+
             //ここにバージョン変更時の処理を書くかもしれない
-            MessageBox.Show("前回起動時からバージョンが変更されました。");
+            //MessageBox.Show("前回起動時からバージョンが変更されました。");\
+            if (now > last_version)
+            {
+                MessageBox.Show("AqtUtlが更新されました。\nv" + last_version + " -> v" + now);
+            }
+            else if(now < last_version)
+            {
+                MessageBox.Show("AqtUtlのバージョンが古いものに置き換えられました。\nv" + last_version + " -> v" + now);
+            }
         }
     }
 }
